44acd741e22d85e5851c210d61c3f66fe2df0c41
PanJoseph <PaulAnJoseph@gmail.com>
2016-05-08 21:59:17 -0400

    The Location Push

--- app/src/main/java/com/fudi/fudi/back/FudiApp.java
+++ app/src/main/java/com/fudi/fudi/back/FudiApp.java
@@ -2,6 +2,7 @@ package com.fudi.fudi.back;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
